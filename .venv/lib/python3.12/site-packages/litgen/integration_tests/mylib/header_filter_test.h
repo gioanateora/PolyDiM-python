74af4a5401802bd31e9eaedc15ff4de03266118d
fabio.vicini <fabio.vicini@polito.it>
2025-09-23 17:32:08 +0200

    rm: vevn

--- .venv/lib/python3.12/site-packages/litgen/integration_tests/mylib/header_filter_test.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// Here, we test that functions placed under unknown preprocessor conditions are not exported by default
-// You could choose to add them anyway with:
-//    options.srcmlcpp_options.header_filter_acceptable_suffixes += "|OBSCURE_OPTION"
-
-#ifdef OBSCURE_OPTION
-MY_API int ObscureFunction() { return 42; }
-#endif
