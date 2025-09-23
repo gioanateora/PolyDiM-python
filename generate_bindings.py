7628048b3fc026d8bb76cf0e9f9772aadd3ccd74
gioanateora <gioana.teora@polito.it>
2025-09-23 22:37:55 +0200

    news

--- generate_bindings.py
+++ generate_bindings.py
@@ -6,42 +6,64 @@ opts = litgen.LitgenOptions()
 # opts.enum_remove_scope = False
 # opts.class_remove_scope = False
 
-# Eigen support (important!)
-opts.srcmlcpp_options.fn_args_resolve_eigen_matrix_to_numpy = True
+
 opts.srcmlcpp_options.header_filter_preprocessor_regions = True
 
-# opts.fn_template_options.add_specialization("Parse", ["std::vector<unsigned int>", "std::vector<double>", "std::set<unsigned int>", "std::vector<int>", "bool", "std::string", "double", "float", "char", "int"], add_suffix_to_function_name=True)
+opts.fn_template_options.add_specialization("Parse", ["std::vector<unsigned int>", "std::vector<double>", "std::set<unsigned int>", "std::vector<int>", "bool", "std::string", "double", "float", "char", "int"], add_suffix_to_function_name=True)
+
+opts.type_replacements.add_replacement("Gedim::IMeshDAO", "Gedim::MeshMatricesDAO")
+
 # opts.fn_template_options.add_specialization("Timer", ["std::chrono::milliseconds"], add_suffix_to_function_name=True)
 
-headers = ["PolyDiM/gedim/GeDiM/src/Geometry/GeometryUtilities.hpp",
+headers = ["PolyDiM/gedim/GeDiM/src/IO/StringsUtilities.hpp",
+           "PolyDiM/gedim/GeDiM/src/Geometry/GeometryUtilities.hpp",
            "PolyDiM/gedim/GeDiM/src/Geometry/MapParallelepiped.hpp",
-            "PolyDiM/gedim/GeDiM/src/Geometry/MapHexahedron.hpp",
-            "PolyDiM/gedim/GeDiM/src/Geometry/MapParallelogram.hpp",
-            "PolyDiM/gedim/GeDiM/src/Geometry/MapQuadrilateral.hpp",
-            "PolyDiM/gedim/GeDiM/src/Geometry/MapTetrahedron.hpp",
-            "PolyDiM/gedim/GeDiM/src/Geometry/MapTriangle.hpp",
+           "PolyDiM/gedim/GeDiM/src/Geometry/MapHexahedron.hpp",
+           "PolyDiM/gedim/GeDiM/src/Geometry/MapTriangle.hpp",
+           "PolyDiM/gedim/GeDiM/src/Geometry/MapParallelogram.hpp",
+           "PolyDiM/gedim/GeDiM/src/Geometry/MapTetrahedron.hpp",
+           "PolyDiM/gedim/GeDiM/src/Geometry/MapQuadrilateral.hpp",
             #"PolyDiM/gedim/GeDiM/src/Mesh/ConformerMeshPolygon.hpp",
             #"PolyDiM/gedim/GeDiM/src/Mesh/ConformerMeshSegment.hpp",
             #"PolyDiM/gedim/GeDiM/src/Mesh/ConformMeshUtilities.hpp",
-            #"PolyDiM/gedim/GeDiM/src/Mesh/IMeshDAO.hpp",
             #"PolyDiM/gedim/GeDiM/src/Mesh/IntersectorMesh2DSegment.hpp",
             #"PolyDiM/gedim/GeDiM/src/Mesh/IntersectorMesh3DSegment.hpp",
-            #"PolyDiM/gedim/GeDiM/src/Mesh/MeshDAOExporterToCsv.hpp",
-            #"PolyDiM/gedim/GeDiM/src/Mesh/MeshDAOImporterFromCsv.hpp",
-            #"PolyDiM/gedim/GeDiM/src/Mesh/MeshFromCsvUtilities.hpp",
-            #"PolyDiM/gedim/GeDiM/src/Mesh/MeshMatrices.hpp",
-            #"PolyDiM/gedim/GeDiM/src/Mesh/MeshMatricesDAO.hpp",
-            #"PolyDiM/gedim/GeDiM/src/Mesh/MeshUtilities.hpp",
+            "PolyDiM/gedim/GeDiM/src/Mesh/MeshDAOExporterToCsv.hpp",
+            "PolyDiM/gedim/GeDiM/src/Mesh/MeshDAOImporterFromCsv.hpp",
+            "PolyDiM/gedim/GeDiM/src/Mesh/MeshFromCsvUtilities.hpp",
+            "PolyDiM/gedim/GeDiM/src/Mesh/MeshMatrices.hpp",
+            "PolyDiM/gedim/GeDiM/src/Mesh/MeshMatricesDAO.hpp",
+            "PolyDiM/gedim/GeDiM/src/Mesh/MeshUtilities.hpp",
             #"PolyDiM/gedim/GeDiM/src/Mesh/ObjectFileFormatInterface.hpp",
             #"PolyDiM/gedim/GeDiM/src/Mesh/OpenVolumeMeshInterface.hpp",
-            #"PolyDiM/gedim/GeDiM/src/Mesh/PlatonicSolid.hpp",
+            "PolyDiM/gedim/GeDiM/src/Mesh/PlatonicSolid.hpp",
             #"PolyDiM/gedim/GeDiM/src/Mesh/RefinementUtilities.hpp",
-            #"PolyDiM/gedim/GeDiM/src/Mesh/SphereMeshUtilities.hpp",
+            "PolyDiM/gedim/GeDiM/src/Mesh/SphereMeshUtilities.hpp",
             #"PolyDiM/gedim/GeDiM/src/Mesh/TetgenInterface.hpp",
             #"PolyDiM/gedim/GeDiM/src/Mesh/TriangleInterface.hpp",
             #"PolyDiM/gedim/GeDiM/src/Mesh/UnionMeshSegment.hpp",
             #"PolyDiM/gedim/GeDiM/src/Mesh/VoroInterface.hpp",
-            #"PolyDiM/gedim/GeDiM/src/Mesh/VtkMeshInterface.hpp"
+            #"PolyDiM/gedim/GeDiM/src/Mesh/VtkMeshInterface.hpp",
+            "PolyDiM/gedim/GeDiM/src/Quadrature/Quadrature_Gauss2D_Triangle.hpp",
+            "PolyDiM/gedim/GeDiM/src/Quadrature/Quadrature_Gauss2D_Square.hpp",
+            "PolyDiM/gedim/GeDiM/src/Quadrature/Quadrature_Gauss3D_Hexahedron.hpp",
+            "PolyDiM/gedim/GeDiM/src/Quadrature/Quadrature_GaussLobatto1D.hpp",
+            "PolyDiM/gedim/GeDiM/src/Quadrature/Quadrature_Gauss1D.hpp",
+            "PolyDiM/gedim/GeDiM/src/Quadrature/QuadratureData.hpp",
+            "PolyDiM/gedim/GeDiM/src/Quadrature/Quadrature_Gauss3D_Tetrahedron_PositiveWeights.hpp",
+            "PolyDiM/gedim/GeDiM/src/Quadrature/Quadrature_Gauss3D_Tetrahedron.hpp",
+            "PolyDiM/PolyDiM/src/Interpolation/lagrange_1D.hpp",
+            #"PolyDiM/PolyDiM/src/VEM/Quadrature/VEM_Quadrature_2D.hpp",
+            #"PolyDiM/PolyDiM/src/VEM/Quadrature/VEM_Quadrature_3D.hpp",
+            #"PolyDiM/PolyDiM/src/VEM/Utilities/VEM_GBasis_2D.hpp",
+            #"PolyDiM/PolyDiM/src/VEM/Utilities/VEM_GBasis_3D.hpp",
+            #"PolyDiM/PolyDiM/src/VEM/Utilities/VEM_GBasis_Data.hpp",
+            #"PolyDiM/PolyDiM/src/VEM/Utilities/VEM_Monomials_1D.hpp",
+            #"PolyDiM/PolyDiM/src/VEM/Utilities/VEM_Monomials_2D.hpp",
+            #"PolyDiM/PolyDiM/src/VEM/Utilities/VEM_Monomials_3D.hpp",
+            #"PolyDiM/PolyDiM/src/VEM/Utilities/VEM_Monomials_Data.hpp",
+            #"PolyDiM/PolyDiM/src/VEM/Utilities/VEM_Monomials_Utilities.hpp",
+            #"PolyDiM/PolyDiM/src/VEM/Utilities/VEM_Inertia_Utilities.hpp"
             ]
 
 # Stampa a console:
--- test.py
+++ test.py
@@ -1,4 +1,16 @@
 import polydim
+import numpy as np
 
 a = polydim.gedim.StringsUtilities.parse_float("6")
 print(a)
+
+b = polydim.gedim.GeometryUtilitiesConfig()
+c = polydim.gedim.GeometryUtilities(b)
+
+origin = np.matrix([[1.0], [2.0], [3.0]], dtype=np.float64)
+print(origin)
+
+mat = c.create_ellipse(2.0, 2.0, 3)
+
+
+print(mat)
